4827d7c4f68cb63eb55981c9015d84fa1c42eedf
Antoine Veuiller <aveuiller@gmail.com>
2015-06-09 19:26:37 +0200

    feature: Update i3 configuration
    - media management input registered (using sink 1 of pulseaudio).
    - New status bar (customised i3pystatus).
    - New exit menu.

---
+++ .i3/i3-status.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+from i3pystatus import Status
+
+status = Status(standalone=True)
+
+# Displays clock like this:
+# Tue 30 Jul 11:59:46 PM KW31
+#                          ^-- calendar week
+status.register("clock",
+    format="%a %-d %b %X KW%V",)
+
+# Shows pulseaudio default sink volume
+#
+# Note: requires libpulseaudio from PyPI
+status.register("pulseaudio",
+    format="{muted} ♪{volume_bar}{volume}",)
+
+# Brightness
+status.register("backlight",
+    format="☀{brightness}/{max_brightness}",)
+
+# This would also display a desktop notification (via dbus) if the percentage
+# goes below 5 percent while discharging. The block will also color RED.
+status.register("battery",
+    format="{status}/{consumption:.2f}W [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}",
+    alert=True,
+    alert_percentage=5,
+    status={
+        "DIS": "↓",
+        "CHR": "↑",
+        "FULL": "=",
+    },)
+
+# Shows the average load of the last minute and the last 5 minutes
+# (the default value for format is used)
+status.register("load")
+
+# Shows your CPU temperature, if you have a Intel CPU
+status.register("temp",
+    format="{temp:.0f}°C",)
+
+# Displays whether a DHCP client is running
+status.register("runwatch",
+    name="DHCP",
+    path="/var/run/dhclient*.pid",)
+
+# Shows the address and up/down state of eth0. If it is up the address is shown in
+# green (the default value of color_up) and the CIDR-address is shown
+# (i.e. 10.10.10.42/24).
+# If it's down just the interface name (eth0) will be displayed in red
+# (defaults of format_down and color_down)
+#
+# Note: the network module requires PyPI package netifaces
+status.register("network",
+    interface="enp3s0",
+    format_up="{v4cidr}",)
+
+# Has all the options of the normal network and adds some wireless specific things
+# like quality and network names.
+#
+# Note: requires both netifaces and basiciw
+status.register("wireless",
+    interface="wlp5s0",
+    format_up="{essid} {v4cidr} {quality:03.0f}%",)
+
+# Shows disk usage of /
+# Format:
+# 42/128G [86G]
+#status.register("disk",
+#    path="/",
+#    format="/ [{avail}G]",)
+#
+#status.register("disk",
+#    path="/home",
+#    format="/home [{avail}G]",)
+#
+
+# Shows mpd status
+# Format:
+# Cloud connected▶Reroute to Remain
+status.register("now_playing",)
+#    format="{title}{status} {song_elapsed}/{song_length}",
+#    status={
+#        "pause": "▷",
+#        "play": "▶",
+#        "stop": "◾",
+#    },)
+
+status.run()
